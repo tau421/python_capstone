503445b11b3062e2dc23585317ce75f8b6ea4659
Kyle Wilson <tau421@gmail.com>
2023-04-10 23:32:09 -0500

    initial commit

---
+++ model.py
@@ -0,0 +1,33 @@
+"""Models for favorite orders app."""
+
+import os
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    """A user."""
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    username = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<User user_id={self.user_id} username={self.username}>'
+    
+def connect_to_db(flask_app, echo=True):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["POSTGRES_URI"]
+    flask_app.config["SQLALCHEMY_ECHO"] = echo
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print("Connected to db.")
+
+if __name__ == "__main__":
+    from server import app
+
+    connect_to_db(app)
