453633c131713d77a6f3b1b8166f0f72c585d3f5
Kyle Wilson <tau421@gmail.com>
2023-04-11 23:35:05 -0500

    models mostly done but with question about classmethods, still adding to server

--- model.py
+++ model.py
@@ -14,6 +14,34 @@ class User(db.Model):
     username = db.Column(db.String, unique=True)
     password = db.Column(db.String)
 
+    """Classmethods"""
+    @classmethod
+    def create_user(cls, username, password):
+        """Create and return a new user."""
+
+        return cls(user=username, password=password)
+    
+    @classmethod
+    def get_user_by_id(cls, user_id):
+        """Return user from id."""
+
+        return cls.query.get(user_id)
+    
+    @classmethod
+    def get_user_by_username(cls, username):
+        """Return user from username."""
+
+        return cls.query.filter(User.username == username).first()
+    
+    @classmethod
+    def get_all_users(cls):
+        return cls.query.all()
+    
+    # def get_restaurants_by_user_id(user_id):
+    #     """Return user's restaurants."""
+
+    #     return Restaurant.query.get(user_id)
+
     def __repr__(self):
         return f'<User user_id={self.user_id} username={self.username}>'
     
@@ -30,6 +58,30 @@ class Restaurant(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
 
     user = db.relationship("User", backref="restaurants")
+
+    """Classmethods."""
+    @classmethod
+    def create_restaurant(cls, user, restaurant_name, menu_link, restaurant_image, is_favorite):
+        """Create and return a new restaurant."""
+
+        return cls(user=user, restaurant_name=restaurant_name, menu_link=menu_link, restaurant_image=restaurant_image, is_favorite=is_favorite)
+
+    @classmethod
+    def get_all_restaurants(cls):
+        """Return all restaurants."""
+
+        return cls.query.all()
+    
+    @classmethod
+    def get_restaurant_by_restaurant_id(cls, restaurant_id):
+        """Return a restaurant by restaurant id."""
+
+        return cls.query.get(restaurant_id)
+
+    # def get_orders_by_restaurant_id(restaurant_id):
+    #     """Return orders from specific restaurant."""
+
+    #     return Order.query.get(restaurant_id)
 
     def __repr__(self):
         return f'<Restaurant restaurant_id={self.restaurant_id} restaurant_name={self.restaurant_name}>'
@@ -48,6 +100,20 @@ class Order(db.Model):
     def __repr__(self):
         return f'<Order order_id={self.order_id} order_description={self.order_description}>'
  
+    """Classmethods."""
+    @classmethod
+    def create_order(cls, restaurant, order_description):
+        """Create and return a new order."""
+
+        return cls(restaurant=restaurant, order_description=order_description)
+    
+    @classmethod
+    def update_order(cls, order_id, new_description):
+        """Update an order given order_id and updated order."""
+
+        order = cls.query.get(order_id)
+        order.order_description = new_description
+
 def connect_to_db(flask_app, echo=True):
     flask_app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["POSTGRES_URI"]
     flask_app.config["SQLALCHEMY_ECHO"] = echo
--- server.py
+++ server.py
@@ -2,14 +2,69 @@
 
 from flask import (Flask, render_template, request, flash, session, redirect)
 
-from model import connect_to_db, db
-import crud
+from model import connect_to_db, db, User, Restaurant, Order
 
 from jinja2 import StrictUndefined
 
 app = Flask(__name__)
 app.secret_key = "dev_mtn_capstone"
 app.jinja_env.undefined = StrictUndefined
+
+@app.route("/")
+def homepage():
+    """View homepage."""
+
+    return render_template("homepage.html")
+
+@app.route("/users")
+def all_users():
+    """View all users."""
+
+    users = User.get_all_users()
+
+    return render_template("all_users.html", users=users)
+
+@app.route("/users", methods=["POST"])
+def register_user():
+    """Create a new user."""
+
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    user = User.get_user_by_username(username)
+    if user:
+        flash("Cannot create an acocunt with that username.  Please try again.")
+    else:
+        user = User.create_user(username, password)
+        db.session.add(user)
+        db.session.commit()
+        flash("Account created!  Please log in.")
+
+    return redirect("/")
+
+@app.route("/users/<user_id>")
+def show_user(user_id):
+    """Show details for a specific user."""
+
+    user = User.get_user_by_id(user_id)
+
+    return render_template("user_details.html", user=user)
+
+@app.route("/login", methods=["POST"])
+def process_login():
+    """Process user login."""
+
+    username = request.form.get("username")
+    password = password.request.get("password")
+
+    user = User.get_user_by_username(username)
+    if not user or user.password != password:
+        flash("The email or password you entered was incorrect.")
+    else:
+        session["user_username"] = user.username
+        flash(f"Welcome back, {user.username}!")
+    
+    return redirect("/")
 
 if __name__ == "__main__":
     connect_to_db(app)
